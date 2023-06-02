40385788b688db4a6d3ada3d96c521b142d6d992
psykrat <129807238+psykrat@users.noreply.github.com>
2023-06-02 16:11:19 -0500

    Update spectralscanner.py

--- spectralscanner.py
+++ spectralscanner.py
@@ -1,6 +1,7 @@
 import json
 import os
 import subprocess
+import xml.etree.ElementTree as ET
 import threading
 import logging
 import sys
@@ -17,15 +18,11 @@ def is_valid_ipv4(ip):
     pattern = re.compile(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
     return bool(pattern.match(ip))
 
-def run_command(command, output_file, timeout=None, dry_run=False):
+def run_command(command, output_file, timeout=None):
     with open(output_file, 'w') as f:
         try:
-            if dry_run:
-                print(f"Dry-run: {command}")
-                return True
-            else:
-                process = subprocess.Popen(command, shell=True, stdout=f, stderr=subprocess.STDOUT)
-                process.communicate(timeout=timeout)
+            process = subprocess.Popen(command, shell=True, stdout=f, stderr=subprocess.STDOUT)
+            process.communicate(timeout=timeout)
         except subprocess.TimeoutExpired:
             logging.error(f"Command '{command}' timed out. Terminating process.")
             process.terminate()
@@ -37,19 +34,23 @@ def run_command(command, output_file, timeout=None, dry_run=False):
 def command_exists(command):
     return subprocess.call("type " + command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
-def run_tools(service, targetURL, output_file, config):
+def run_tools(port_service, targetIP, output_file, config):
+    port, service = port_service
+
     if service == 'http':
-        logging.info("HTTP service found. Running dirb and nikto...")
-        executor.submit(run_command, f"dirb {quote(targetURL)}", f"{output_file}_dirb.txt", timeout=config['dirb']['timeout'])
-        executor.submit(run_command, f"nikto -h {quote(targetURL)}", f"{output_file}_nikto.txt", timeout=config['nikto']['timeout'])
+        targetURL = f"http://{targetIP}:{port}"
+        logging.info(f"HTTP service found on port {port}. Running dirb and nikto...")
+        run_command(f"dirb {quote(targetURL)}", f"{output_file}_dirb_{port}.txt", timeout=config['dirb']['timeout'])
+        run_command(f"nikto -h {quote(targetURL)}", f"{output_file}_nikto_{port}.txt", timeout=config['nikto']['timeout'])
 
     elif service == 'ssh':
-        logging.info("SSH service found. Running hydra...")
-        executor.submit(run_command, f"hydra -l {quote(config['hydra']['username'])} -P {quote(config['hydra']['passlist'])} ssh://{quote(targetURL)}", f"{output_file}_hydra.txt", timeout=config['hydra']['timeout'])
+        targetURL = f"ssh://{targetIP}:{port}"
+        logging.info(f"SSH service found on port {port}. Running hydra...")
+        run_command(f"hydra -l {quote(config['hydra']['username'])} -P {quote(config['hydra']['passlist'])} {quote(targetURL)}", f"{output_file}_hydra_{port}.txt", timeout=config['hydra']['timeout'])
 
     elif service == 'smb' or service == 'netbios-ssn':
         logging.info("SMB service found. Running enum4linux...")
-        executor.submit(run_command, f"enum4linux {quote(targetIP)}", f"{output_file}_enum4linux.txt", timeout=config['enum4linux']['timeout'])
+        run_command(f"enum4linux {quote(targetIP)}", f"{output_file}_enum4linux.txt", timeout=config['enum4linux']['timeout'])
 
 def parse_nmap_output(file_path):
     services = []
@@ -60,8 +61,9 @@ def parse_nmap_output(file_path):
     for line in lines:
         if "/tcp" in line:
             service_info = line.split()
+            port = service_info[0].split("/")[0]
             service = service_info[2]
-            services.append(service)
+            services.append((port, service))
 
     return services
 
@@ -80,11 +82,9 @@ def main(targetIP, projectName, dry_run=False, log_file='script.log'):
         return
 
     # Define file names for output
+    xmlOutput = f"{projectName}.xml"
     txtOutput = f"{projectName}.txt"
     output_prefix = f"{projectName}"
-
-    # Define the URL for tools
-    targetURL = f"http://{targetIP}"
 
     # Check that required tools are installed
     required_tools = ['nmap', 'nbtscan', 'whois', 'theHarvester', 'dirb', 'nikto', 'hydra', 'enum4linux']
@@ -95,16 +95,16 @@ def main(targetIP, projectName, dry_run=False, log_file='script.log'):
 
     # Run the nmap command
     logging.info("Running nmap command...")
-    if not run_command(f"sudo nmap -T4 -sC -sV -oN {quote(txtOutput)} {quote(targetIP)}", txtOutput, timeout=config['nmap']['timeout'], dry_run=dry_run):
+    if not run_command(f"sudo nmap -T4 -sC -sV -oX {quote(xmlOutput)} {quote(targetIP)} > {quote(txtOutput)}", xmlOutput, timeout=config['nmap']['timeout']):
         logging.error("Nmap command failed. Exiting.")
         return
 
-    # Parse the nmap output to check for services
+    # Parse the nmap output and run appropriate tools
     logging.info("Parsing nmap output and running appropriate tools...")
     services = parse_nmap_output(txtOutput)
 
-    for service in services:
-        executor.submit(run_tools, service, targetURL, output_prefix, config)
+    for port_service in services:
+        executor.submit(run_tools, port_service, targetIP, output_prefix, config)
 
     # Wait for all threads to finish
     executor.shutdown(wait=True)
